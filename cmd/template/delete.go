4d2acad436eb2b432414d59dd369556a2bc69009
aaronv <aarongeovanny70@gmail.com>
2026-05-28 18:27:09 -0600

    Template: Add delete command

---
+++ cmd/template/delete.go
@@ -0,0 +1,28 @@
+package template
+
+import (
+	"fmt"
+
+	"github.com/aaron70/decoy-cli/cli"
+	"github.com/spf13/cobra"
+)
+
+func createDeleteCommand(cli *cli.CLI) *cobra.Command {
+	command := &cobra.Command{
+		Use: "delete <name>",
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			name := args[0]
+
+			entity, err := cli.TemplateSvc.Delete(name)
+			if err != nil {
+				return err
+			}
+
+			fmt.Fprintf(cmd.OutOrStdout(), "Template %q has been deleted:\n%s\n", entity.Name, entity.Tmpl)
+			return nil
+		},
+	}
+
+	return command
+}
