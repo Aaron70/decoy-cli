50df412767e4b2c8a77a83e166302ccf6d927905
aaronv <aarongeovanny70@gmail.com>
2026-05-28 18:27:36 -0600

    Template: Include all template commands

--- cmd/template/template.go
+++ cmd/template/template.go
@@ -12,6 +12,9 @@ func CreateTemplateCommand(cli *cli.CLI) *cobra.Command {
 	}
 
 	command.AddCommand(createStoreCommand(cli))
+	command.AddCommand(createGetCommand(cli))
+	command.AddCommand(createDeleteCommand(cli))
+	command.AddCommand(createParseCommand(cli))
 
 	return command
 }
