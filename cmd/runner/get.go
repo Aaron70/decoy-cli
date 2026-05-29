8a37f8b4a9730eceb46f9ef057814f3260a2f7a5
aaronv <aarongeovanny70@gmail.com>
2026-05-28 21:18:33 -0600

    Runner: Add get command

---
+++ cmd/runner/get.go
@@ -0,0 +1,35 @@
+package runner
+
+import (
+	"fmt"
+
+	"github.com/aaron70/decoy-cli/cli"
+	"github.com/spf13/cobra"
+)
+
+func createGetCommand(cli *cli.CLI) *cobra.Command {
+	command := &cobra.Command{
+		Use:   "get [<name>]",
+		Args:  cobra.MaximumNArgs(1),
+		Short: "List all the runners or get the details of the given runner.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 0 {
+				entity, err := cli.RunnerSvc.Get(args[0])
+				if err != nil {
+					return err
+				}
+				fmt.Fprintf(cmd.OutOrStdout(), "%s\n", string(entity.Config))
+			} else {
+				entities, err := cli.RunnerSvc.GetAll()
+				if err != nil {
+					return err
+				}
+				for _, entity := range entities {
+					fmt.Fprintf(cmd.OutOrStdout(), "%s\n", entity.Name)
+				}
+			}
+			return nil
+		},
+	}
+	return command
+}
