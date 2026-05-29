86b599f65ae00e92763b18f89773430bbc6d3a39
aaronv <aarongeovanny70@gmail.com>
2026-05-28 18:27:23 -0600

    Template: Add parse command

---
+++ cmd/template/parse.go
@@ -0,0 +1,87 @@
+package template
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aaron70/decoy"
+	"github.com/aaron70/decoy-cli/cli"
+	"github.com/spf13/cobra"
+)
+
+func createParseCommand(cli *cli.CLI) *cobra.Command {
+	var (
+		name, tmpl, file, data string
+		err                    error
+		stringValues           map[string]string
+	)
+
+	command := &cobra.Command{
+		Use:  "parse [<name>]",
+		Args: cobra.MaximumNArgs(1),
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) > 0 {
+				name = args[0]
+				if tmpl != "" {
+					fmt.Fprintf(cmd.ErrOrStderr(), "[Warning]: Ignoring --template/-t flag since the template name %q was provided.", name)
+				}
+				if file != "" {
+					fmt.Fprintf(cmd.ErrOrStderr(), "[Warning]: Ignoring --file/-f flag since the template name %q was provided.", name)
+				}
+				entity, err := cli.TemplateSvc.Get(name)
+				if err != nil {
+					return err
+				}
+				tmpl = entity.Tmpl
+			} else if cmd.Flags().Changed("file") {
+				bytes, err := os.ReadFile(file)
+				if err != nil {
+					return err
+				}
+				tmpl = string(bytes)
+			} else if !cmd.Flags().Changed("template") {
+				tmpl, err = cli.ReadStringFrom(cmd.InOrStdin())
+				if err != nil {
+					return fmt.Errorf("Couldn't read the template from stdin: %w", err)
+				}
+			}
+
+			var jsonData map[string]any
+			err := json.Unmarshal([]byte(data), &jsonData)
+			if err != nil {
+				return fmt.Errorf("Couldn't deserialize the contents from the flag --data/-d: %w", err)
+			}
+
+			for key, value := range stringValues {
+				jsonData[key] = value
+			}
+
+			parsedTemplate := bytes.NewBufferString("")
+
+			err = cli.Decoy.ParseTemplate(parsedTemplate, tmpl,
+				decoy.WithFuncMap(decoy.Default.DefaultTemplateFuncMaps()),
+				decoy.WithData(jsonData),
+				decoy.WithTemplateNamed(name),
+			)
+			if !strings.HasSuffix(parsedTemplate.String(), "\n") {
+				fmt.Fprintf(cmd.OutOrStdout(), "%s\n", parsedTemplate.String())
+			} else {
+				fmt.Fprint(cmd.OutOrStdout(), parsedTemplate.String())
+			}
+			return err
+		},
+	}
+
+	command.Flags().StringVarP(&tmpl, "template", "t", "", "The content of the template to parse")
+	command.Flags().StringVarP(&file, "file", "f", "", "The path of the file with the template contents to parse")
+	command.MarkFlagsMutuallyExclusive("template", "file")
+
+	command.Flags().StringVarP(&data, "data", "d", "{}", "The JSON data to be used within the template")
+	command.Flags().StringToStringVarP(&stringValues, "value", "v", map[string]string{}, "A set of pairs (key=value) to inject into the data")
+
+	return command
+}
